0cab790b9d43bc8f02607faff182d079d061f298
mikael04 <mikael.coletto.eng@gmail.com>
2021-10-04 19:43:35 -0300

    Resolvendo primeiro exercício; Organizando outra pasta para meus exercícios; Criando segundo app, mas não feito;

--- Material-curso-r/exercicios/010-meu-primeiro-app.R
+++ Material-curso-r/exercicios/010-meu-primeiro-app.R
@@ -15,3 +15,68 @@
 # ou rode 
 # 
 # ggplot2::diamonds
+
+library(shiny)
+library(bs4Dash)
+library(ggplot2)
+
+func_get_numeric_columns <- function(df){
+  col_names <- colnames(df)
+  col_names_ <- ""
+  j = 1
+  for(i in 1:length(col_names)){
+    if(is.numeric(df[[i]])){
+      col_names_[j] <- col_names[i]
+      j = j+1
+    }
+  }
+}
+
+## df usado
+df <- ggplot2::diamonds
+## traz apenas colunas numéricas
+col_names <- func_get_numeric_columns(df)
+
+
+ui <- bs4Dash::dashboardPage(
+  title = "Primeiro exercício",
+  header = dashboardHeader(),
+  sidebar = bs4Dash::dashboardSidebar(),
+  controlbar = dashboardControlbar(),
+  footer = dashboardFooter(),
+  body = bs4Dash::dashboardBody(
+    # shinyWidgets::pickerGroupUI()
+    shiny::selectInput(inputId = "variavel",
+                       label = "Escolha a variável",
+                       choices = names(df),
+                       multiple = F,
+                       selectize = T,
+                       selected = NULL
+                       ),
+    plotOutput("hist_plot")
+    
+    
+  )
+)
+
+server <- function(input, output, session){
+  # summary(df)
+  observeEvent(input$variavel, {
+    # req(input$variavel)
+    # validate(!is.numeric(df$carat), "Variável não numérica")
+    output$hist_plot <- renderPlot(
+      ggplot2::ggplot(df, ggplot2::aes(x = get(input$variavel))) +
+        ggplot2::geom_histogram(stat = "count",
+                                col="lightblue", 
+                                fill="green", 
+                                alpha = .2) + 
+        ggplot2::labs(title= paste0("Histograma da variavel: ", input$variavel), x=input$variavel, y="Count") +
+        ggplot2::theme_minimal()
+      # xlim(c(18,52)) + 
+      # ylim(c(0,30))
+    )
+    
+  })
+}
+
+shinyApp(ui = ui, server = server)

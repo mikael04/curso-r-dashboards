aeb7760db28c6bcca64f1707a87f0435378d7fb6
mikael04 <mikael.coletto.eng@gmail.com>
2021-10-16 07:48:03 -0300

    Resolvendo alguns exercícios;

--- Material-curso-r/exercicios/031-navbarPage.R
+++ Material-curso-r/exercicios/031-navbarPage.R
@@ -7,3 +7,50 @@
 #
 # - a segunda com a série temporal da média diária do último mês da base 
 # permitindo a escolha do poluente.
+
+library(shiny)
+library(bs4Dash)
+library(ggplot2)
+library(dplyr)
+library(plotly)
+library(shinyWidgets)
+# library(shinydashboard) # <-- Change this line to: library(semantic.dashboard)
+
+# df <- readRDS("Material-curso-r/dados/cetesb.rds")
+df <- readRDS("../dados/cetesb.rds")
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Basic dashboard"),
+  dashboardSidebar(sidebarMenu(
+    menuItem(tabName = "med_oz", text = "Média diária Oz", icon = icon("home")),
+    menuItem(tabName = "med_mes_ant", text = "Média mês anterior", icon = icon("tree"))
+  )),
+  dashboardBody(
+    fluidRow(
+      br(),
+      h4("Tabela de crimes para o mês selecionado"),
+      reactable::reactableOutput("table"),
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  set.seed(122)
+  teste = F
+  # df_auxx <- shiny::eventReactive(input$selecionar_data, {
+  #   # browser()
+  #   df
+  # })
+  df_aux <- shiny::eventReactive(input$date, {
+    df
+  })
+  output$table <- reactable::renderReactable({
+    # browser()
+    df_ <- df_aux()
+    reactable::reactable(df_)
+    
+  })
+}
+
+shinyApp(ui, server)
+
